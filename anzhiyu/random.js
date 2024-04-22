f92418c54f2ab45a21f0e0828f89f49300a4d16a
LiJiaTu-M-Tang <152180368+LiJiaTu-M-Tang@users.noreply.github.com>
2024-04-22 10:45:28 +0800

    Site updated: 2024-04-22 10:45:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/10/hello-world/","2024/04/10/article/"];function toRandomPost(){
+var posts=["2024/04/22/hello-my-friend/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
