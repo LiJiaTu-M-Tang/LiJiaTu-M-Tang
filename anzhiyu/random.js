c432b4aa744e99f0ce95836cf1f89b8bc5d5fd1b
LiJiaTu-M-Tang <152180368+LiJiaTu-M-Tang@users.noreply.github.com>
2024-04-28 09:30:45 +0800

    Site updated: 2024-04-28 09:30:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/22/hello-my-friend/"];function toRandomPost(){
+var posts=["2024/04/22/hello-my-friend/","2024/04/27/python魔法棒和魔法书之间的关系/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
